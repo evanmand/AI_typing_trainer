e8de07acdf64a4765e658d28ef7b52eb52d42195
Evan Anderson <evanmand02@gmail.com>
2023-11-13 12:04:22 -0700

    Tracking keystrokes

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
+def keystroke(event):
+    key = event.char
+    print(f"'{key}' key pressed")
+
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!",font=("Arial", 25)).grid(column=0, row=0)
+
+prompt_text = "The quick brown fox jumps over the lazy dog"
+
+root.bind('<Key>', keystroke)
+text_var = prompt_text
+text = ttk.Label(frm, text=text_var,font=("Arial", 25)).grid(column=0, row=0)
 root.attributes('-fullscreen', True)
+text_var = prompt_text
 root.mainloop()
