d51153a99d68e206ec490d2107b55d7b6f5bc93e
Evan Anderson <evanmand02@gmail.com>
2023-11-23 12:08:49 -0700

    Still working on typing

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-prompt_text = "The quick brown fox jumps over the lazy dog"
-
-text_var = prompt_text
-text = ttk.Label(frm, text=text_var,font=("Arial", 25)).grid(column=0, row=0)
+def create_label():
+    prompt_text = "The quick brown fox jumps over the lazy dog"
+    text_var = prompt_text
+    text = ttk.Label(frm, text=text_var,font=("Arial", 25)).grid(column=0, row=0)
+   
 
 def clear_frame():
    for widgets in root.winfo_children():
@@ -17,10 +18,11 @@ def clear_frame():
 def keystroke(event):
     key = event.char
     print(f"'{key}' key pressed")
-    clear_frame()
-    text = ttk.Label(frm, text=key,font=("Arial", 25)).grid(column=0, row=0)
+    # clear_frame()
+    create_label()
+    # text = ttk.Label(frm, text=key,font=("Arial", 25)).grid(column=0, row=0)
 root.bind('<Key>', keystroke)
 
 root.attributes('-fullscreen', True)
-text_var = prompt_text
+# text_var = prompt_text
 root.mainloop()
