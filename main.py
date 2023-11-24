77c6df6da111dd6627b6d080f30dac005841a28d
Evan Anderson <evanmand02@gmail.com>
2023-11-23 21:13:01 -0700

    Aligning text

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+root.grid_columnconfigure(0, weight = 0)
+root.grid_columnconfigure(1, weight = 1)
+root.grid_rowconfigure(0, weight = 1)
+
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
 def create_label(input_text, row):
-    ttk.Label(frm, text=input_text,font=("Arial", 25)).grid(column=0, row=row)
+    label = ttk.Label(frm, text=input_text, font=("Arial", 25))
+    label.grid(column = 0,
+               row = row,
+               sticky = W)
 
 def clear_frame():
    for widgets in root.winfo_children():
       widgets.destroy()
 
 string_thing = ""
+create_label("", 0) # Placeholder before user starts typing
 create_label("Prompt text", 1)
 
 def keystroke(event):
