d818b562e84e10d0cc53ecd11c2c240fab5eee11
Evan Anderson <evan.anderson@usu.edu>
2023-11-29 17:10:43 -0700

    Created main loop

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import ttk
+from openai import OpenAI
+
+# client = OpenAI()
 
 root = Tk()
 root.grid_columnconfigure(0, weight = 0)
 root.grid_columnconfigure(1, weight = 1)
 root.grid_rowconfigure(0, weight = 0)
 root.grid_rowconfigure(1, weight = 1)
-root.attributes("-fullscreen", False)
-root.update()
+root.attributes('-fullscreen', False)
 
 frm = ttk.Frame(root, padding=10)
 frm.grid()
@@ -23,8 +25,6 @@ def clear_frame():
       widgets.destroy()
 
 string_thing = ""
-create_label("", 0) # Placeholder before user starts typing
-create_label("ChatGPT (Chat Generative Pre-trained Transformer) is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, that enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language. Successive prompts and replies, known as prompt engineering, are considered at each conversation stage as a context.", 1)
 
 def keystroke(event):
     global string_thing
@@ -36,10 +36,17 @@ def keystroke(event):
     string_thing = string_thing + key
     create_label(string_thing, 0)
 
-
-    # text = ttk.Label(frm, text=key,font=("Arial", 25)).grid(column=0, row=0)
 root.bind('<Key>', keystroke)
 
-root.attributes('-fullscreen', True)
-# text_var = prompt_text
+def main():
+    print("hehe")    # text = ttk.Label(frm, text=key,font=("Arial", 25)).grid(column=0, row=0)
+
+create_label("", 10) # Placeholder before user starts typing
+create_label("ChatGPT (Chat Generative Pre-trained Transformer) is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, that enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language. Successive prompts and replies, known as prompt engineering, are considered at each conversation stage as a context.", 1)
+restart = ttk.Button(frm, text="Restart", command=main)
+restart.grid()
+quit = ttk.Button(frm, text="Quit", command=quit)
+quit.grid()
+    
+main()
 root.mainloop()
